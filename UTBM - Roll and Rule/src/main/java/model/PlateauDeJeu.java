724dff7e185d2ad543aceee0311d50df30c18c75
Esteban Gomez <109286055+Jinupse@users.noreply.github.com>
2024-12-29 18:38:46 +0100

    Update PlateauDeJeu.java

--- UTBM - Roll and Rule/src/main/java/model/PlateauDeJeu.java
+++ UTBM - Roll and Rule/src/main/java/model/PlateauDeJeu.java
@@ -14,7 +14,7 @@ public class PlateauDeJeu {
         this.roue = new RoueDesSemestres();
         this.feuillesJoueurs = new ArrayList<>();
 
-        // Création des feuilles de joueur
+        // Création des feuilles de joueurs
         for (int i = 1; i < nombreJoueurs + 1; i++) {
             Scanner myObj = new Scanner(System.in);  // Create a Scanner object
             System.out.println("Entrez le nom du joueur " + i + " ");
@@ -22,11 +22,9 @@ public class PlateauDeJeu {
             //System.out.println("Nom du joueur " + i + ": ");  // Output user input
 
             this.feuillesJoueurs.add(new FeuilleDeJoueur(name));
-            for (FeuilleDeJoueur joueur : this.feuillesJoueurs) {
-                System.out.println(joueur.getName());
-                // petit souci je sais pas pourquoi à chaque tour la boucle à l'air de passer par
-                // tous les elements de la liste genre la à chaque tour ca print tous les noms
-            }
+        }
+        for (FeuilleDeJoueur joueur : this.feuillesJoueurs){
+            System.out.println(joueur.getName());
         }
     }
 
