50b6bcb0cdd5f7e1d581edb3ccfc437610f9d3f0
Esteban Gomez <109286055+Jinupse@users.noreply.github.com>
2025-01-02 18:02:35 +0100

    Update RoueDesSemestres.java

--- UTBM - Roll and Rule/src/main/java/model/RoueDesSemestres.java
+++ UTBM - Roll and Rule/src/main/java/model/RoueDesSemestres.java
@@ -1,6 +1,7 @@
 package main.java.model;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class RoueDesSemestres {
     protected int numeroTour;
@@ -11,7 +12,34 @@ public class RoueDesSemestres {
         this.numeroTour = 0;
         this.salles = new ArrayList<>();; //Initialiser les 9 salles
         this.des = new ArrayList<>();; //Initialiser les 4 dés
-        //Il faut rajouter ici l'initialisation des objets précédents !
+
+        // Initialisation des salles
+        this.salles.add(new Salle(0,0));
+        this.salles.add(new Salle(0,2));
+        this.salles.add(new Salle(0,1));
+        this.salles.add(new Salle(2,0));
+        this.salles.add(new Salle(2,2));
+        this.salles.add(new Salle(1,0));
+        this.salles.add(new Salle(1,2));
+        this.salles.add(new Salle(1,2));
+        this.salles.add(new Salle(1,1));
+
+        Random rand = new Random();
+        for (Salle instance_salle : this.salles){
+            int rand_retourner = rand.nextInt(2);
+            if (rand_retourner == 1){
+                instance_salle.retourner();
+                // les prints en dessous servent pour le debug si besoin de verifier quelle salle est retournée
+                //System.out.println("Verso : " + instance_salle.getSecteur());
+            } else {
+                // System.out.println("Recto : " + instance_salle.getSecteur());
+            }
+        }
+
+        for (int i = 1; i < 3 + 1; i++) {
+            this.des.add(new De(true));
+        }
+        this.des.add(new De(false));
     }
     public void nouveauTour() {
         if (numeroTour < 16) {
